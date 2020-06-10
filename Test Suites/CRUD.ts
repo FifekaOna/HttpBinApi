89eea5374789592abbfb998a9f357e1df74bfb3e
ona <ona@qerja.com>
2020-06-11 02:43:58 +0700

    Create CRUD testcases , Create Test Suite

---
+++ Test Suites/CRUD.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CRUD</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0f30edb8-59cb-42e7-b29e-6796111f9a6f</testSuiteGuid>
+   <testCaseLink>
+      <guid>34157bde-cde3-40c9-9ca8-bbfcf83b6825</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRUD/Create</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>042c71c7-dcfb-4bd3-8078-75a1ea3168a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRUD/Update</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e1a2cf6-1d81-4578-9d73-62209451e44d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRUD/Delete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e95713a-6e96-4897-ac08-16b4c206c412</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRUD/Read</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
